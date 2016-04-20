5b4013c082aa5ef225e4d3c30da01b3976b22c8a
junmorimoto <morimoto.jun@workhappiness.co.jp>
2016-04-20 11:43:13 +0000

    challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ActiveRecord::Base
+  # 年齢は必須入力かつ数字で０以上
+  validates :age , numericality: { only_integer: true , greater_than_or_equal_to: 0 } , presence: true
   # 名前は必須入力かつ20文字以内
   validates :name , length: { maximum: 20 } , presence: true
   # 内容は必須入力かつ2文字以上30文字以下
